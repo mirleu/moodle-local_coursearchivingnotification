333eb4150b801d7161c9d320652dd61b4cbe954a
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2025-02-24 19:11:31 -0800

    initial check-in.

---
+++ README.md
@@ -0,0 +1,13 @@
+# moodle-local_coursearchivingnotification
+
+This plugin adds a banner-alert to the top of each course page, if the course has an even ID.
+
+_It exists only to illustrate how content can be injected into the page header without custom theming._
+
+## Attribution
+
+Most of this was cobbled together with parts lifted from other plugins, most notably the [Course Ratings](https://github.com/marinaglancy/moodle-tool_courserating) plugin.
+
+## License
+
+http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
---
+++ classes/local/hooks/output/before_footer_html_generation.php
@@ -0,0 +1,57 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+namespace local_coursearchivingnotification\local\hooks\output;
+
+use local_coursearchivingnotification\output\renderer;
+
+/**
+ * Hook callback for local_coursearchivingnotification.
+ *
+ * @package    local_coursearchivingnotification
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class before_footer_html_generation
+{
+
+    /**
+     * A callback to add markup to the page's main content area, right before the footer.
+     *
+     * @param \core\hook\output\before_footer_html_generation $hook
+     */
+    public static function callback(\core\hook\output\before_footer_html_generation $hook): void
+    {
+        global $PAGE;
+
+        // Ignore hook during installation or upgrades.
+        if (during_initial_install() || isset($CFG->upgraderunning)) {
+            return;
+        }
+
+        /** @var renderer $output */
+        $output = $PAGE->get_renderer('local_coursearchivingnotification');
+
+        $rhett = '';
+        // Only render the notification if we're on a course page,
+        // and if the course has an even number for an ID.
+        if ($PAGE->course
+            && 0 === $PAGE->course->id % 2) {
+            $rhett .= $output->widget();
+        }
+        $hook->add_html($rhett);
+    }
+}
