f0fb4f5016b5132f8a470b383deb4d02359cbc2e
Miriam Leung <miriam.leung@ucsf.edu>
2026-02-10 13:52:59 -0800

    Updates banner message

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025072500;
+$plugin->version   = 2026021000;
 $plugin->requires  = 2024100700;
 $plugin->component = 'local_coursearchivingnotification';
 $plugin->release = 'v4.5';
